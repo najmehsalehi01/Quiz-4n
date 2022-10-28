f681baddc3572d72fac73f9059de5978dc56abc2
dkhongell <116841735+dkhongell@users.noreply.github.com>
2022-10-28 12:52:33 -0400

    Add files via upload

---
+++ Q41.py
@@ -0,0 +1,17 @@
+Python 3.10.6 (v3.10.6:9c7b4bd164, Aug  1 2022, 17:13:48) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+def count_hashtag(string):
+    if count_hashtag=="hashtag":
+        print("number of hashtags available")
+
+
+
+def hash_spam(string):
+    if number of hash_spam=< 4:
+        
+SyntaxError: invalid syntax
+def hash_spam(string):
+    if number of #hashtags are used to make comments
+    
+SyntaxError: invalid syntax
+#i don't understand how we are supposed to do this 
