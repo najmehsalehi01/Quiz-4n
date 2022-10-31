07791ad018b4516ed8add36a128f7cb2d2f9e476
Najmeh Salehi <117115789+najmehsalehi01@users.noreply.github.com>
2022-10-31 15:04:55 -0400

    Modify your codes!
    Please use the count_substring function in the slides and modify your function!

--- Q41.py
+++ Q41.py
@@ -1,6 +1,6 @@
-Python 3.10.6 (v3.10.6:9c7b4bd164, Aug  1 2022, 17:13:48) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
+
 def count_hashtag(string):
+    # use the codes and functions that are availabe in your slides and modify these codes!
     if count_hashtag=="hashtag":
         print("number of hashtags available")
 
